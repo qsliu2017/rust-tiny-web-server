6e0afaa7511503bcab798914921e72520c910209
qsliu <qsliu2017@outlook.com>
2022-06-14 11:43:16 +0800

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{
+    io::{Read, Write},
+    net,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = net::TcpListener::bind("127.0.0.1:2017").unwrap();
+    match listener.accept() {
+        Ok((mut stream, addr)) => {
+            println!("Accept conn from {addr:?}");
+            let mut buf = [0; 1024];
+
+            stream.read(&mut buf).unwrap_or_else(|e| panic!("{e}"));
+            println!("Got stream: {buf:?}");
+
+            write!(&mut stream, "HTTP/1.0 200 OK\r\n\r\nHello World\r\n")
+                .unwrap_or_else(|e| panic!("{e}"));
+            stream.flush().unwrap_or_else(|e| panic! {"{e}"});
+        }
+        Err(e) => panic!("{}", e),
+    }
 }
